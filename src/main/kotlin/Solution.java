127883f6d9eb1bd65cf2672a67c16684a5356d27
stevenhao <stevenhaoit@gmail.com>
2023-05-05 13:55:11 +0800

    ///摆动排序

--- src/main/kotlin/Solution.java
+++ src/main/kotlin/Solution.java
@@ -13,4 +13,31 @@ public class Solution {
         }
         return res;
     }
+
+    ///摆动排序
+    ///属于数学抽象
+    /*
+    这个坑在于替换如何保证之前已排序好的规则不变
+     */
+    public void wiggleSort(int[] nums) {
+        boolean less = true;
+        for (int i = 0; i < nums.length - 1; i ++) {
+            if (less) {
+                if (nums[i] > nums[i + 1]) {
+                    swap(nums, i, i + 1);
+                }
+            } else {
+                if (nums[i] < nums[i + 1]) {
+                    swap(nums, i, i + 1);
+                }
+            }
+            less = !less;
+        }
+    }
+
+    void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
 }
